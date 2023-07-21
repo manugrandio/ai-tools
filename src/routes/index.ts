f762bde478402423f37759527e160ae6ce9fa449
Manu Grandio <manugrandio@fastmail.fm>
2023-07-21 09:32:17 +0200

    add rabbitmq and publisher

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,16 @@
 import { Router, RequestHandler, Request, Response } from "express";
+import client, { Connection, Channel } from "amqplib";
 
-const helloWorld = (_: Request, res: Response) => {
+const helloWorld = async (_: Request, res: Response) => {
+  const connection: Connection = await client.connect(
+    'amqp://username:password@rabbitmq:5672'
+  )
+
+  const channel: Channel = await connection.createChannel()
+  await channel.assertQueue('testQueue');
+  channel.sendToQueue('testQueue', Buffer.from('hello world from queue'));
+  connection.close();
+
   res.status(201).send({ "msg": "hello world" });
 };
 
